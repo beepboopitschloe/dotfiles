a1f8bb5aece3d54b98ed439c462d4953a8f4d793
Noah Muth <noah.muth@code42.com>
2015-11-02 09:23:54 -0600

    Add .tmux.conf to symlinks in setup.sh.

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 ln -s $(pwd)/.vimrc ~/.vimrc
 ln -s $(pwd)/.vim ~/.vim
 ln -s $(pwd)/.bash_profile ~/.bash_profile
+ln -s $(pwd)/.tmux.conf ~/.tmux.conf
 
 # make sure that .env_vars exists (it gets sourced by the bash profile)
 touch ~/.env_vars.sh
