7a3ef96ff4275d1201af36cbfb0202d52f3b6732
Noah Muth <noahmuth@gmail.com>
2015-12-02 08:27:05 -0600

    Add support for neovim to setup.sh.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 # set up symbolic links to files in this repo
-ln -s $(pwd)/.vimrc ~/.vimrc
 ln -s $(pwd)/.vim ~/.vim
+ln -s $(pwd)/.vimrc ~/.vimrc
+mkdir ~/.config
+ln -s $(pwd)/.vim ~/.config/nvim
+ln -s $(pwd)/.vimrc ~/.config/nvim/init.vim
 ln -s $(pwd)/.bash_profile ~/.bash_profile
 
 # make sure that .env_vars exists (it gets sourced by the bash profile)
