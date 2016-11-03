74c6fdfb74777f9da6bbbc5c8de3229b44d66edc
Noah Muth <ext.nmuth@riotgames.com>
2016-11-02 22:22:29 -0500

    Elm formatting stuff.

--- new-emacs-setup/elm.el
+++ new-emacs-setup/elm.el
@@ -1,14 +1,23 @@
 (message "configuring elm...")
 
 (defun nmuth/elm-mode-hook ()
+  (interactive)
   (elm-oracle-setup-completion)
   (setq company-backend 'company-elm))
 
 (use-package elm-mode :ensure t
   :config
   (add-to-list 'company-backends 'company-elm)
-  (add-hook 'elm-mode-hook #'nmuth/elm-mode-hook))
+  (add-hook 'elm-mode-hook #'nmuth/elm-mode-hook)
+  (setq elm-format-on-save t))
 
 (use-package flycheck-elm :ensure t
   :config
   (shell-command "yarn global add elm-oracle"))
+
+(general-define-key :states '(normal visual insert emacs)
+                    :keymaps 'elm-mode-map
+                    :prefix "SPC"
+                    :non-normal-prefix "C-c"
+
+                    "j =" 'elm-mode-format-buffer)
