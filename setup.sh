d3b63814540e023edbb4d036a10cb65069cfdb6b
Noah.Muth <a612857@f0189897a042.target.com>
2019-03-28 17:22:46 -0500

    Change tmux prefix to C-Space; tweak setup.sh.

--- setup.sh
+++ setup.sh
@@ -1,14 +1,15 @@
 # set up symbolic links to files in this repo
-ln -s $(pwd)/.vim ~/.vim
-ln -s $(pwd)/.vimrc ~/.vimrc
-mkdir ~/.config
-ln -s $(pwd)/.vim ~/.config/nvim
-ln -s $(pwd)/.vimrc ~/.config/nvim/init.vim
-ln -s $(pwd)/.bash_profile ~/.bash_profile
-ln -s $(pwd)/.tmux.conf ~/.tmux.conf
-ln -s $(pwd)/.spacemacs ~/.spacemacs
-ln -s $(pwd)/layers ~/.emacs.d/private
-ln -s $(pwd)/.hyper.js ~/.hyper.js
+ln -nfs $(pwd)/.vim ~/.vim
+ln -nfs $(pwd)/.vimrc ~/.vimrc
+mkdir -p ~/.config
+ln -nfs $(pwd)/.vim ~/.config/nvim
+ln -nfs $(pwd)/.vimrc ~/.config/nvim/init.vim
+ln -nfs $(pwd)/.bash_profile ~/.bash_profile
+ln -nfs $(pwd)/.tmux.conf ~/.tmux.conf
+ln -nfs $(pwd)/.spacemacs ~/.spacemacs
+ln -nfs $(pwd)/.emacs.d ~/.emacs.d
+ln -nfs $(pwd)/layers ~/.emacs.d/private
+ln -nfs $(pwd)/.hyper.js ~/.hyper.js
 
 # make sure that .env_vars exists (it gets sourced by the bash profile)
 touch ~/.env_vars.sh
